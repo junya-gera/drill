3f55e236b1dabccb12e6f6151df95a3bbc248121
junya-gera <aa106921@gmail.com>
2020-04-25 23:38:41 +0900

    hashの中身をeachで表示

---
+++ drill7.rb
@@ -0,0 +1,19 @@
+hash = {}
+
+hash[:name] = "Suzuki"
+hash[:height] = 1.6
+hash[:weight] = 60
+# hash[:bmi] = hash[:weight] / hash[:height] **2
+hash[:bmi] = (hash[:weight] / hash[:height] **2).round(1)
+
+# puts "name: #{hash[:name]}
+# height: #{hash[:height]}
+# weight: #{hash[:weight]}
+# bmi: #{hash[:bmi].round(1)}"
+
+hash.each do |key,value|
+  puts "#{key}: #{value}"
+end
+
+# option + ¥ でバックスラッシュ
+
