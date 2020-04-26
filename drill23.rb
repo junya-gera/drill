6c4cddc1682eb2c4e600151e1d943fb0710a8554
junya-gera <aa106921@gmail.com>
2020-04-26 17:17:36 +0900

    23break,array.pop

---
+++ drill23.rb
@@ -0,0 +1,19 @@
+a = 0
+
+while true do
+ puts a
+ a += 1
+ if a > 100
+  break
+ end
+end
+
+
+# 以下の配列arrayに対してあるメソッドを実行し、
+# 最後の値"something"を配列から削除してください
+
+array = ["dog", "cat", "something"]
+
+array.pop
+
+puts array
