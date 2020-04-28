391cadc20d159500733029b72b98ea4d184bc8fb
junya-gera <aa106921@gmail.com>
2020-04-29 06:39:02 +0900

    58 if-else問題 weekday

---
+++ drill58.rb
@@ -0,0 +1,14 @@
+def sleep_in(weekday,vacation)
+
+  if weekday != true && vacation == true
+    puts "true"
+  else
+    puts "false"
+  end
+
+end
+
+is_weekday = false
+is_vacation = true
+
+sleep_in(is_weekday,is_vacation)
