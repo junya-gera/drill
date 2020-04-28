cfcf05ab0d708c56004cfd2260a8fabb0424787f
junya-gera <aa106921@gmail.com>
2020-04-29 05:49:09 +0900

    32 fizzbuzz応用 if文1行

---
+++ drill32.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(number)
+  i = 0
+  while i <= number do
+    str = ""
+    str = str + "fizz" if i % 3 == 0
+    str = str + "buzz" if i % 5 == 0
+    str = str + i.to_s if str == ""
+    puts str
+    i += 1
+  end
+end
+
+fizzbuzz(15)
